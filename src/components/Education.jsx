dd7e0024a9aa5d3da34938d8929f8db048466d30
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-07-15 13:30:05 +1000

    added edit and save mode for education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,66 +2,76 @@ import { useState } from "react";
 
 function Education({eduExps, setEduExps, onAddFormEdu}) {
 
+  const [disableMode, setDisableMode] = useState(true);
+
+  function handleDisableMode(e) {
+    setDisableMode(!disableMode)
+  }
+
   return (
-    <fieldset>
-      <legend>Educational Experience</legend>
-      {eduExps.map((eduItem, index) => (
-        <div key={index}>
-          <label>
-            School Name:
-            <input
-              type="text"
-              name="name"
-              value={eduItem.name}
+    <div>
+      <fieldset>
+        <legend>Educational Experience</legend>
+        {eduExps.map((eduItem, index) => (
+          <div key={index}>
+            <label>
+              School Name:
+              <input
+                type="text"
+                name="name"
+                value={eduItem.name}
+                onChange={(event) => setEduExps(index, event)}
+              />
+            </label>
+            <label>
+              Degree Title:
+              <input
+                type="text"
+                name="degreeTitle"
+                value={eduItem.price}
+                onChange={(event) => setEduExps(index, event)}
+              />
+            </label>
+            <label>
+              Degree Level:
+              <select
+                name="degreeLevel"
+                value={eduItem.degreeLevel}
+                onChange={(event) => setEduExps(index, event)}
+              >
+                <option value="">Select Level</option>
+                <option value="High School">High School</option>
+                <option value="Diploma">Diploma</option>
+                <option value="Bachelor">Bachelor</option>
+                <option value="Master">Master</option>
+                <option value="Doctorate">Doctorate</option>
+              </select>
+            </label>
+            <label>
+              Start Date: 
+              <input type="date"
+              name="fromDate"
+              value={eduItem.fromDate}
               onChange={(event) => setEduExps(index, event)}
-            />
-          </label>
-          <label>
-            Degree Title:
-            <input
-              type="text"
-              name="degreeTitle"
-              value={eduItem.price}
+              />
+            </label>
+            <label>
+              Graduation Date: 
+              <input type="date"
+              name="toDate"
+              value={eduItem.toDate}
               onChange={(event) => setEduExps(index, event)}
-            />
-          </label>
-          <label>
-            Degree Level:
-            <select
-              name="degreeLevel"
-              value={eduItem.degreeLevel}
-              onChange={(event) => setEduExps(index, event)}
-            >
-              <option value="">Select Level</option>
-              <option value="High School">High School</option>
-              <option value="Diploma">Diploma</option>
-              <option value="Bachelor">Bachelor</option>
-              <option value="Master">Master</option>
-              <option value="Doctorate">Doctorate</option>
-            </select>
-          </label>
-          <label>
-            Start Date: 
-            <input type="date"
-            name="fromDate"
-            value={eduItem.fromDate}
-            onChange={(event) => setEduExps(index, event)}
-            />
-          </label>
-          <label>
-            Graduation Date: 
-            <input type="date"
-            name="toDate"
-            value={eduItem.toDate}
-            onChange={(event) => setEduExps(index, event)}
-            />
-          </label>
-        </div>
-      ))}
-      <button type="button" onClick={onAddFormEdu}>
-        +
-      </button>
-    </fieldset>
+              />
+            </label>
+          </div>
+        ))}
+        <button type="button" onClick={onAddFormEdu}>
+          +
+        </button>
+      </fieldset>
+      <button disabled={disableMode} onClick={handleDisableMode}>Save</button>
+      <button disabled={!disableMode} onClick={handleDisableMode}>Edit</button>
+    </div>
   )
 }
 
